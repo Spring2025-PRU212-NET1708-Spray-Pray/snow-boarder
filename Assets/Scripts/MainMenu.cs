b50bc9d73d7c84d9e688baf62a094541a5a9334a
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-03-06 19:48:50 +0700

    feat: add a help panel

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject helpPanel; // Reference to the Help Panel
+    [SerializeField] GameObject startButton; // Reference to the Start Button
+    [SerializeField] GameObject helpButton;
+    [SerializeField] GameObject quitButton; // Reference to the Quit Button
+
     public void StartGame()
     {
         SceneManager.LoadScene("Level1"); // Replace "GameScene" with the name of your game scene
@@ -10,8 +15,19 @@ public class MainMenu : MonoBehaviour
 
     public void Help()
     {
-        // Implement options menu functionality here
+        helpPanel.SetActive(true); // Show the Help Panel
+        startButton.SetActive(false); // Hide the Start Button
+        helpButton.SetActive(false); // Hide the Help Button
+        quitButton.SetActive(false); // Hide the Quit Button
         Debug.Log("Help button clicked");
+    }
+
+    public void CloseHelp()
+    {
+        helpPanel.SetActive(false); // Hide the Help Panel
+        startButton.SetActive(true); // Show the Start Button
+        helpButton.SetActive(true); // Show the Help Button
+        quitButton.SetActive(true); // Show the Quit Button
     }
 
     public void ExitGame()
